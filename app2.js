8ce88e6fe9bc39fd75edc889328601c5004b15ef
elziorocha <rochasenzo.04@gmail.com>
2023-10-03 22:12:28 -0300

    falta o css

--- app2.js
+++ app2.js
@@ -9,9 +9,10 @@ app.set('views', __dirname + '/views');
 
 // configurar o body-parser para processar os dados do form
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static(__dirname + '/public'));
 
 //blog
-const post = [
+const posts = [
     {
         id: 1,
         title: 'Primeira Postagem',
@@ -27,4 +28,28 @@ const post = [
 //Rota principal
 app.get('/', (req, res) => {
     res.render('index', { posts });
+});
+
+//Rota para exibir uma postagem individual
+app.get('/post/:id', (req, res) => {
+    const id = req.params.id;
+    const post = posts.find(post => post.id === parseInt(id));
+    res.render('post', { post });
+});
+
+//Rota para exibir o formulário de adição de post
+app.get('/add', (req, res) => {
+    res.render('add');
+});
+
+//Rota para processar a adição da postagem
+app.post('/add', (req, res) => {
+    const { title, content } = req.body;
+    const id = posts.length + 1;
+    posts.push({id, title, content});
+    res.redirect('/');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
