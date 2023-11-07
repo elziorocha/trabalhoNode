331b2cb86776002ac3f5b8afca5fd24883021b51
elzio <rochasenzo.04@gmail.com>
2023-11-07 03:06:55 -0300

    autenticação com middleware

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const app = express();
+const port = 3333;
 
-app.use(logger);
+// middleware global (para todas as páginas) (troca de página e horário)
+app.use(logger, (req, res, next) => {
+    const currentDate = new Date();
+    console.log(`Solicitação recebida em: ${currentDate}`);
+    next();
+});
 
 app.get('/', (req, res) => {
-    res.send('Home Page')
+    res.send('Home')
 })
 
 app.get('/users', auth, (req, res) => {
-    res.send('Users Page')
+    console.log(`Usuário admin = ${req.admin}`)
+    console.log('Página do Usuário')
+    res.send('Página do Usuário')
 })
 
 function logger(req, res, next) {
-    console.log('Log')
+    console.log('usuário trocou de página')
     next()
 }
 
 function auth(req, res, next) {
-    console.log('Auth')
-    next()
+    if (req.query.admin === 'true') {
+        req.admin = true
+        next()
+        return
+    } else {
+        res.send('Sem Autenticação')
+    }
 }
 
 app.listen(3333, () => {
-    console.log('Console iniciado')
+    console.log(`Console iniciado na porta http://localhost:${port}`)
 });
