31407c2c62e9e32c21d2178246a95c9500c80d66
elzio <rochasenzo.04@gmail.com>
2023-11-11 17:16:38 -0300

    correção nos testes unitários e página sobre

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,18 +1,17 @@
 const request = require('supertest');
 const app = require('../app');
 
-// teste unitário para retorno de HTML das páginas
 describe('GET /', () => {
   test('Deve retornar o HTML da página Home', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toContain(`<html>
+    expect(response.text).toMatchSnapshot(`<html>
     <head>
         <link rel="stylesheet" type="text/css" href="/style.css">
     </head>
 
         <header class="header">
-            <h1>Nando Company</h1>
+            <a href="/"><h1>Nando Company</h1></a>
 
             <nav>
                 <a href="/sobre">Sobre</a>
@@ -31,6 +30,38 @@ describe('GET /', () => {
                     <a href="/post_usuario">Suas Postagens</a>
                 </nav>
             </div>
+        </div>`);
+  });
+});
+
+describe('GET /sobre', () => {
+  test('Deve retornar o HTML da página Sobre', async () => {
+    const response = await request(app).get('/sobre');
+    expect(response.status).toBe(200);
+    expect(response.text).toMatchSnapshot(`<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/style.css">
+    </head>
+
+        <header class="header">
+            <a href="/"><h1>Nando Company</h1></a>
+
+            <nav>
+                <a href="/sobre">Sobre</a>
+                <a href="/contato">Contato</a>
+                <a href="/login">Login</a>
+            </nav>
+        </header>
+
+        <div class="caixa_sobre">
+            <h2>Sobre a Nando Company</h2>
+            <p>A Nando Company Entertainment surgiu a partir da ideia de reunir diversos
+            recursos de entreterinemto em um mesmo lugar, seja filmes, jogos, séries, etc. </p>
+        </div>
+
+        <div class="caixa_sobre">
+            <p>O Website foi lançado em 2023, reunindo primeiramente uma coletânea de jogos para
+            usuários avaliar suas experiências durante a jogatina.</p>
         </div>`);
   });
 });
--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.get('/', (req, res) => {
     </head>
 
         <header class="header">
-            <h1>Nando Company</h1>
+            <a href="/"><h1>Nando Company</h1></a>
 
             <nav>
                 <a href="/sobre">Sobre</a>
@@ -126,6 +126,36 @@ app.get('/', (req, res) => {
                     <a href="/post_usuario">Suas Postagens</a>
                 </nav>
             </div>
+        </div>
+    `)
+})
+
+app.get('/Sobre', (req, res) => {
+    res.send(`
+    <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/style.css">
+    </head>
+
+        <header class="header">
+            <a href="/"><h1>Nando Company</h1></a>
+
+            <nav>
+                <a href="/sobre">Sobre</a>
+                <a href="/contato">Contato</a>
+                <a href="/login">Login</a>
+            </nav>
+        </header>
+
+        <div class="caixa_sobre">
+            <h2>Sobre a Nando Company</h2>
+            <p>A Nando Company Entertainment surgiu a partir da ideia de reunir diversos
+            recursos de entreterinemto em um mesmo lugar, seja filmes, jogos, séries, etc. </p>
+        </div>
+
+        <div class="caixa_sobre">
+            <p>O Website foi lançado em 2023, reunindo primeiramente uma coletânea de jogos para
+            usuários avaliar suas experiências durante a jogatina.</p>
         </div>
     `)
 })
